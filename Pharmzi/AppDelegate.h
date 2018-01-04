d8b11a4ae5d339b07df57bcfd1484ef6c9155136
apple <apple@arjun.local>
2018-01-04 10:37:49 +0530

    Initial Commit

---
+++ Pharmzi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pharmzi
+//
+//  Created by apple on 04/01/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
